dcbc97e74835c8d53c3feaeb0ed332d467d91f4d
AnaMaria <annamarinescu0@gmail.com>
2021-05-19 14:43:35 +0300

    Unit Testing

---
+++ Laborator12- Testare unitara/src/ro/ase/csie/cts/g1087/testare/main/Aplicatie.java
@@ -0,0 +1,28 @@
+package ro.ase.csie.cts.g1087.testare.main;
+
+import ro.ase.csie.cts.g1087.testare.modele.OperatiiMatematice;
+
+public class Aplicatie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello. Aplicatie pornita");
+		
+//		int a=5;
+//		int b=5;
+//		int rezultatasteptat=10;
+//		int rezultatcalculat=OperatiiMatematice.aduna(a, b);
+//		
+//		if(rezultatasteptat==rezultatcalculat) {
+//			System.out.println("ok");
+//		}
+//		
+//		else
+//		{
+//			System.out.println("nok");
+//		}
+		
+	}
+
+}
